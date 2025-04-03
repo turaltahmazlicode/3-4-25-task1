c25cfc408b3a6c7c81f3ead94181d260de3c536f
nigarasadzada <nigarza-bp219@code.edu.az>
2025-04-03 19:13:53 +0400

    Add project files.

---
+++ 3-4-25-task1/ExtentionHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp;
+
+public static class ExtentionHelper
+{
+    public static bool IsAlpha(this string str)
+    {
+        if (Regex.IsMatch(str, @"^[A-Za-z\s]+$"))
+            return true;
+        return false;
+    }
+    public static bool IsValidPassword(this string str)
+    {
+        if (Regex.IsMatch(str, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$"))
+            return true;
+        return false;
+    }
+    public static bool IsValidUsername(this string str)
+    {
+        if (Regex.IsMatch(str, @"^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$"))
+            return true;
+        return false;
+    }
+    public static bool IsValidDate(this DateOnly date)
+    {
+        if (date.Year >= 1970)
+            return true;
+        return false;
+    }
+    public static string Capitalize(this string str)
+    {
+        return new StringBuilder(char.ToUpper(str[0]))
+            .Append(str[1..]
+            .ToLower())
+            .ToString();
+    }
+}
+
+
---
+++ 3-4-25-task1/Person.cs
@@ -0,0 +1,81 @@
+﻿namespace ConsoleApp;
+
+internal class Person
+{
+    #region constructors
+    public Person(string? name, string? surname, string? username, string? passsword, DateOnly birthday)
+    {
+        Name = name;
+        Surname = surname;
+        Username = username;
+        Passsword = passsword;
+        Birthday = birthday;
+    }
+    #endregion
+
+    #region fields
+    private string? _name;
+    private string? _surname;
+    private string? _username;
+    private string? _passsword;
+    private DateOnly _birthday;
+    #endregion
+
+    #region properties
+    public string? Name
+    {
+        get => _name;
+        set
+        {
+            if (value.IsAlpha())
+                _name = value.Capitalize();
+            throw new Exception();
+        }
+    }
+    public string? Surname
+    {
+        get => _surname;
+        set
+        {
+            if (value.IsAlpha())
+                _surname = value.Capitalize();
+            throw new Exception();
+        }
+    }
+    public string? Username
+    {
+        get => _username;
+        set
+        {
+            if (value.IsValidUsername())
+                _username = value.Capitalize();
+            throw new Exception();
+        }
+    }
+    public string? Passsword
+    {
+        get => _passsword;
+        set
+        {
+            if (value.IsValidPassword())
+                _passsword = value;
+            throw new Exception();
+        }
+    }
+    public DateOnly Birthday
+    {
+        get => _birthday;
+        init
+        {
+            if (value.IsValidDate())
+                _birthday = value;
+            throw new Exception();
+        }
+    }
+    public byte Age => (byte)(DateTime.Now.Year - Birthday.Year);
+
+    #endregion
+
+    public override string ToString() => $"Name: {Name}\nSurname: {Surname}\nUsername: {Username}\nAge: {Age}";
+
+}
